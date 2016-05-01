fd621a3134a8fa6b32e4060e34e9de8d64d878df
Ovidio R. Calvet <calveto@gmail.com>
2016-04-30 21:25:42 -0400

    Created a http server to respond on port 8383

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello Isma');
+var portNumber = 8383;
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.write("Hello there");
+  res.end();
+});
+
+server.listen(portNumber);
+
+console.log('Server listening on port ', portNumber);
